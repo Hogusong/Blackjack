5a3852598d30ef380b30d0a67f48d305b911c179
“Young <hogusong1@gmail.com>
2019-07-30 15:54:21 -0400

    Create 'player controller' and add a function to handle LocalStorage in it. Define main Variables.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './css/styles.css';
+import PLAYER from './models/player';
+import * as ctrl from './controller/player';
+
+let playersBase = ctrl.createPlayers();     // Get players' infomation from the LocalStorage.
+let players = [], currIndex = -1, timer, delayTime = 3000;
+let gameResult = [];
+const dealer = new PLAYER('Dealer', 100000000, true);   // Create the dealer.
+let inAddOrRemove = false, gameStarted = false;
+console.log(playersBase);
