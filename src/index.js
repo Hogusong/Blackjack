332228a02a7b3794ba12b1ef3c970775f04b690a
“Young <hogusong1@gmail.com>
2019-07-30 16:40:46 -0400

    Create initial view to let players decide 'in-play' or 'stay out'.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import PLAYER from './models/player';
 import * as ctrl from './controller/player';
 import * as base from './models/base';
 import { dom, message } from './models/base';
+import { settingPlayer } from './views/initialView';
 
 let playersBase = ctrl.createPlayers();     // Get players' infomation from the LocalStorage.
 let players = [], currIndex = -1, timer, delayTime = 3000;
@@ -21,6 +22,7 @@ function init() {
   inAddOrRemove = false;          // to control buttons -> Start game, Add, Remove
   gameStarted = false;            // to control the status for in Game or not.
   dom.dScore.innerText = ' -- last score : ' + dealer.getScore();
+  settingPlayer(playersBase);         // Show available players in the table
 
   dom.btnAdd.onclick = () => {
     inAddOrRemove = true;
