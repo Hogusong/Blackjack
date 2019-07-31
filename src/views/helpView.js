2dade145ad845fd08db99ffbd84ed4bd02700dd1
“Young <hogusong1@gmail.com>
2019-07-31 01:24:13 -0400

    Add 'Game Help'.

--- src/views/helpView.js
+++ src/views/helpView.js
@@ -1,9 +1,11 @@
 import { dom } from '../models/base';
 import charts from '../images/BJCharts.png';
+import help from '../images/GameHelp.png';
 
 export const renderGameHelp = () => {
   dom.gameRule.style.display = 'block';
   dom.blackjackGuide.style.display = 'none';
+  document.querySelector('#game-rule img').src = help;
 }
 
 export const renderGuide = () => {
