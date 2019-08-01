4cda48541f530fe4c6b1d88a483d2e1671d77756
“Young <hogusong1@gmail.com>
2019-08-01 00:40:19 -0400

    Create 'Help Controller' to manipulate the game configuration.

--- src/controller/help.js
+++ src/controller/help.js
@@ -5,21 +5,36 @@ export const setEventForHelp = () => {
   dom.modalStart.style.display = 'block';
   help.renderGuide();
   setEvent();
+}
 
-  function setEvent() {
-    dom.btnGuide.onclick = () => {
-      help.renderGuide();
-      setEvent();
-    }
-    dom.btnGameHelp.onclick = () => {
-      help.renderGameHelp();
-      setEvent();
-    }
-    dom.btnOption.onclick = () => {
-      help.renderOptions();
-    }
-    dom.btnExit.onclick = () => {
-      dom.modalStart.style.display = 'none';
+function setEvent() {
+  dom.btnGuide.onclick = () => {
+    help.renderGuide();
+    setEvent();
+  }
+  dom.btnGameHelp.onclick = () => {
+    help.renderGameHelp();
+    setEvent();
+  }
+  dom.btnExit.onclick = () => {
+    dom.modalStart.style.display = 'none';
+  }
+
+  dom.btnOption.onclick = configuration;
+}
+
+function configuration() {
+  let config = JSON.stringify(localStorage.getItem('bj-config'));
+  if (!config) {
+    config = {
+      showResult: true,
+      delay: '2000',
+      keepInPlay: true,
+      keepLastBet: true,
+      minBetting: '5',
+      howManyDecks: '6'
     }
   }
+  help.renderOptions();
+
 }
